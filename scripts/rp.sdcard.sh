4408a876ff2467b6fe546d886e1962f5757c9a57
ChngJeremy <chng_jeremy@outlook.com>
2022-07-09 23:58:39 +0800

    added more boot options

--- scripts/rp.sdcard.sh
+++ scripts/rp.sdcard.sh
@@ -4,6 +4,11 @@
 
 # $1 = imagefile name
 # $2 = sdcard (device file)
+# $3 = partition (number)
+# $4 = boot partition (number)
+# $5 = root partition (number)
+# $6 = swap partition (number)
+# $7 = imagefile size
 # Otherwise defaults to the defaults, see below.
 
 DEFAULTbs=4M                                 # Block size, 4 mb default.
@@ -12,6 +17,10 @@ DEFAULTsdcard="/dev/mmcblk0"                 # SD card device file.
 ROOTUSER_NAME=root                           # User name for the root account.
 E_NOTROOT=81
 E_NOIMAGE=82
+E_NOSDCARD=83
+E_NOPARTITION=84
+E_NOBUFFER=85
+E_NOBUILTIN=86
 
 username=$(id -nu)                           # Get user name.
 if [ "$username" != "$ROOTUSER_NAME" ]
@@ -20,10 +29,19 @@ then
   exit $E_NOTROOT
 fi
 
-if [ -n "$1" ]
+if [ -n "$1" ]                            # If image file name is specified.
+then
+  if [ -f "$1" ]                            # If image file exists.
+  then
+    DEFAULTif="$1"
+  else
+    echo "Image file $1 does not exist."
+    exit $E_NOIMAGE
+  fi
+fi
 then
   imagefile="$1"
-else
+else    
   imagefile="$DEFAULTif"
 fi
 
@@ -34,23 +52,56 @@ else
   sdcard="$DEFAULTsdcard"
 fi
 
-if [ ! -e $imagefile ]
+if [ -n "$3" ]
 then
-  echo "Image file \"$imagefile\" not found!"
+  partition="$3"
+else
+  partition="1"
+fi
+
+if [ -n "$4" ]
+then
+  bootpartition="$4"
+else
+  bootpartition="1"
+fi
+
+if [ -n "$5" ]
+then
+  rootpartition="$5"
+else
+  rootpartition="2"
+fi
+
+if [ -n "$6" ]
+then
+  swappartition="$6"
+else
+  swappartition="3"
+fi
+
+if [ -n "$7" ]
+then
+  imagesize="$7"
+else
+  imagesize="$(stat -c%s "$imagefile")"
+fi
+
+
+if [ ! -e $imagefile ]                    # If image file does not exist.
+then
+  echo "Image file $imagefile does not exist."
   exit $E_NOIMAGE
 fi
 
-echo "Last chance to change your mind!"; echo
-read -s -n1 -p "Hit a key to write $imagefile to $sdcard [Ctl-c to exit]."
-echo; echo
+echo "Last chance to change your mind!"; echo   
+read -s -n1 -p "Hit a key to write $imagefile to $sdcard [Ctl-c to exit]."  # Prompt for confirmation.
+echo; echo 
+
 
 echo "Writing $imagefile to $sdcard ..."
 dd bs=$DEFAULTbs if=$imagefile of=$sdcard
+echo "Write Done."
 
-exit $?
+exit $?                                 # Exit with last command status.
 
-# Exercises:
-# ---------
-# 1) Provide additional error checking.
-# 2) Have script autodetect device file for SD card (difficult!).
-# 3) Have script sutodetect image file (*img) in $PWD.
